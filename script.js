fce7e4bcebdb9889ba63cb3de486720d33b5d962
Sheremeta Ivan <sheremetavanya2004@gmail.com>
2023-06-14 16:33:23 +0300

    add-cycles-to-code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = Number(prompt("How many films do you watched?", ""));
+const numberOfFilms = Number(prompt("How many films did you watched?", ""));
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,12 +8,24 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("What the latest film do you watch?"),
-  b = prompt("How would you rate it?"),
-  c = prompt("What the latest film do you watch?"),
-  d = prompt("How would you rate it?");
+for (let i = 0; i < 2; i += 1) {
+  const a = prompt("What the latest film do you watch?"),
+    b = prompt("How would you rate it?");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i -= 1;
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+  console.log("Watched very few films!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("You're a classical watcher!");
+} else if (personalMovieDB.count >= 30) {
+  console.log("You are so movie buff!");
+} else {
+  console.log("ERROR!!!");
+}
 
 console.log(personalMovieDB);
